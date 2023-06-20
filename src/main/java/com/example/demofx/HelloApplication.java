e7afc2c734aae8d483bc7f6f5416a3e786671483
Nguyen Trong Tien <nguyentrongtiencntt@gmail.com>
2023-06-20 13:44:10 +0700

    btnClose.setOnAction((e)-> Platform.exit());

--- src/main/java/com/example/demofx/HelloApplication.java
+++ src/main/java/com/example/demofx/HelloApplication.java
@@ -1,8 +1,11 @@
 package com.example.demofx;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +14,17 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Button btnClose = new Button("Close");
+
+        VBox vBox = fxmlLoader.load();
+
+        vBox.getChildren().add(btnClose);
+
+        Scene scene = new Scene(vBox, 320, 240);
+
+        btnClose.setOnAction((e)-> Platform.exit());
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
